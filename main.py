9d61614bc6ff4a1eaa3a5d91c2c16e19529e371f
NikosZoros3533 <nikozoro@csd.auth.gr>
2023-02-07 21:44:37 +0200

    rquestions

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 from stackapi import StackAPI
+import json
 
-SITE = StackAPI('stackoverflow')
+SITE = StackAPI('stackoverflow', key="z4*7kJUg2KkWHjeqU4N7zw((")
+rquestions = SITE.fetch('questions', tagged='R')
+print(len(rquestions['items']))
+print(rquestions)
