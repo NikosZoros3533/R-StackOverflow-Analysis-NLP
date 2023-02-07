2329ac7eaaedf5aef3ebfc47f083a83f449d44fd
NikosZoros3533 <nikozoro@csd.auth.gr>
2023-02-07 21:49:00 +0200

    pageting

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from stackapi import StackAPI
 import json
 
 SITE = StackAPI('stackoverflow', key="z4*7kJUg2KkWHjeqU4N7zw((")
+SITE.page_size = 4800
+SITE.max_pages = 100
 rquestions = SITE.fetch('questions', tagged='R')
-print(len(rquestions['items']))
-print(rquestions)
+#print(len(rquestions['items']))
+#print(rquestions)
