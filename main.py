c78aeb649d00f2d6ab55ab6ade78fbf660e9607e
NikosZoros3533 <nikozoro@csd.auth.gr>
2023-02-12 02:43:23 +0200

    functions complete

--- main.py
+++ main.py
@@ -7,76 +7,173 @@ import time
 
 """
 TODO
--->make a function that start from startpage  (with pagesize  100) and go up till maxpagesize=100
-and collect 10000 questions.
-
--->call that function until the property "hasmore"=false 
 
 -->dump in a jsonfile
 rquestions = SITE.fetch('questions', tagged='R',page=....)
+use data dump
 """
 
 
-"""This function takes as parameter the page it should start and fetch the next 10k questions and returns them"""
-def fetch_bath_questions(startpage):
+
+
+
+ids = set()
+
+
+
+"""
+This function takes as parameter the page it should start and fetch the next maxpages x pagesize
+questions and returns them
+ """
+def fetch_batch_questions(startpage):
     tempquestions = SITE.fetch('questions', tagged='R', page=startpage)
-    return tempquestions['items']
+    return tempquestions
 
 
 
 
-"""This function takes as a parameter the starting page and increment it by"""
-def get_next_pages(pages):
-    pages += 100
-    return pages
 
 
-"""This function fetch all 480000 questions"""
-def fetch_all_data(rquestions):
-    startpage = 0
-    for x in range(2):
-        pages = get_next_pages(startpage)
-        tempquestions = fetch_bath_questions(pages)
-        rquestions.extend(tempquestions)
-        """time.sleep(2)"""
-    print(pages)
 
-"""This function takes as a parameter a dict of questions and extract all the owners of them"""
-def create_owners(questions):
-    tempowners = []
-    for x in range(len(questions)):
-        tempowners.append(questions[x]["owner"])
-    return tempowners
+
+
+
+"""
+This takes as a parameter the data that we collect from the API and extact and return a list of unique users after it
+saves their ids in the global set of ids 
+"""
+def extract_unique_users(data):
+    unique_users = []
+    global ids
+
+    for item in data["items"]:
+        user = item["owner"]
+        idkey = item["owner"]["user_id"]
+        if idkey not in ids:
+            unique_users.append(user)
+        ids.add(idkey)
+
+    return unique_users
+
+
+
+
+
+
+
+"""This function fetch all  questions and returns them ,as well as all the unique owners of these"""
+def fetch_all_data():
+    startpage = 1
+    fetching = True
+    questions = []
+    owners = []
+
+    while fetching:
+        data = fetch_batch_questions(startpage)
+        print("fetching...")
+        owners.extend(extract_unique_users(data))
+        questions.extend(data["items"])
+        has_more = data["has_more"]
+        backoff = data["backoff"]
+        quota_remaining = data["quota_remaining"]
+        if not has_more or len(questions) >= 480000:
+            return questions, owners
+        if quota_remaining <= 3:
+            time.sleep(3600)
+            print("sleeping for the day")
+        if backoff:
+            time.sleep(backoff+1)
+            print("sleeping for a sec")
+        time.sleep(1)
+        startpage += 100
+
+
+
+
+"""
+This is the same as fetch_all_data function that is testing for a smaller 
+amount of data
+"""
+def testing_fetch_all_data():
+    startpage = 1
+    fetching = True
+    questions = []
+    owners = []
+
+    while fetching:
+        data = fetch_batch_questions(startpage)
+        print("fetching...")
+        owners.extend(extract_unique_users(data))
+        questions.extend(data["items"])
+        has_more = data["has_more"]
+        backoff = data["backoff"]
+        quota_remaining = data["quota_remaining"]
+        if not has_more or len(questions) >= 480:
+            return questions, owners
+        if quota_remaining <= 3:
+            time.sleep(3600)
+            print("sleeping for the day")
+        if backoff:
+            time.sleep(backoff+1)
+            print("backing off")
+        print("sleeping for a sec")
+        time.sleep(1)
+        startpage += 10
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 SITE = StackAPI('stackoverflow', key="z4*7kJUg2KkWHjeqU4N7zw((")
-SITE.page_size = 2
-SITE.max_pages = 1
+SITE.page_size = 10
+SITE.max_pages = 10
 
-questions = []
+questionsR, ownersofR = testing_fetch_all_data()
 
-fetch_all_data(questions)
+with open("questionsR.json", "w") as f:
+    json.dump(questionsR, f)
+
+with open("ownersofR.json", "w") as f2:
+    json.dump(ownersofR, f2)
+
+print(len(questionsR))
+print(len(ownersofR))
 
 
-owners = create_owners(questions)
-print(questions)
-print(owners)
 
-print(len(questions))
-print(len(owners))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 """
-
-with open("4questions.json", "w") as f:
+with open("questions.json", "w") as f:
     json.dump(questions, f)
-
-print(len(questions['items']))
 """
+
+
 
 
 
--- second.py
+++ second.py
@@ -1,11 +1,20 @@
 import json
 
-with open('4questions.json') as json_f:
+
+
+
+"""
+with open('example.json') as json_f:
     questions4 = json.load(json_f)
 
+"""
 
 
-owners = []
-for x in range(len(questions4)):
-    owners.append(questions4[x]["owner"])
-print(owners)
+
+
+
+
+
+
+
+
